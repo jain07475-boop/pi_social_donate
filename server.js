bee04b00864770afca77fafece113b76018553a0
jain07475-boop <jain07475@gmail.com>
2025-08-24 13:28:05 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import donationRoutes from "./routes/donationRoutes.js"
